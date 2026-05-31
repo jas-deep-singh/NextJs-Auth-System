843eb658e9a9928ffa9c96f7c22893844fd735fa
Jasdeep Singh <jasdeepsingh6864@gmail.com>
2026-05-31 15:53:29 +0530

    add logout functionality

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,34 @@
+import { connectDB } from '@/dbConfig/dbConfig';
+import User from '@/models/userModel.js';
+import { NextRequest, NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+    try {
+        const reqBody = await request.json();
+        const { username, email, password } = reqBody;
+        const user = await User.findOne({email});
+        if(user) {
+            return NextResponse.json({error: 'User already exists'}, {status: 400});
+        }
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+        const savedUser = await newUser.save();
+        return NextResponse.json({
+            message: 'User created successfully',
+            success: true,
+            savedUser
+        });
+    } catch(error: unknown) {
+        const message = error instanceof Error ? error.message : 'Internal Server Error';
+        return NextResponse.json({error: message}, {status: 500});
+    }
+}
+
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,25 @@
+"use client"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+
 export default function ProfilePage() {
+    const router = useRouter();
+    const logout = async() => {
+        try {
+            await axios.get('/api/users/logout');
+            router.push('/login');
+        } catch(error: unknown) {
+            const message = error instanceof Error ? error.message : 'Internal server error';
+            console.log(message);
+        }
+    }
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <hr />
+            <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4">Logout</button>
         </div>
     )
 }
